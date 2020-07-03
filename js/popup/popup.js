b6e72b222ba1af7778429f06d48774de3978e07f
isso0424 <isso04240129@gmail.com>
2020-07-04 07:52:48 +0900

    [Add]残りの課題が0個の際の処理を追加

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -7,6 +7,11 @@ function update() {
     document.getElementById("app").innerHTML = "<h3>課題がインポートされていません</h3><h4>TODOの画面でリロードすることで課題を更新します</h4>"
     return
   }
+
+  if (assignments.length === 0) {
+    document.getElementById("app").innerHTML = "<h3>残っている課題はありません</h3><h4>もしうまく反映されていなければTODOの画面でリロードしてください。</h4>"
+    return
+  }
   
   assignments.forEach((value, index) => {
     element += `<div class="task" id=assignment${index}><p>name: ${value.name}</p><p>limit: ${value.limit}</p><p>subject: ${value.subject}</p></div>`
