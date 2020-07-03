29624615fb8b8f0629c26c7ae7550b56c6b40ef5
isso0424 <isso04240129@gmail.com>
2020-07-04 03:51:42 +0900

    [Add]課題の名前と科目名、期限を取得した

--- contentScript.js
+++ contentScript.js
@@ -7,15 +7,73 @@ async function loadElement(){
     console.log(assignments)
     
     let elements = assignments.children;
-    let limits = [];
+    let limit = "";
+    let assignment_contents = []
     
     for (let i = 0; i < elements.length; i++) {
-      console.log(elements[i]);
-      if (elements[i].className === "WN8ale i9glDf")
-        limits.push(elements[i].textContent)
+      if (elements[i].className === "WN8ale i9glDf") {
+        limit = elements[i].textContent
+        continue
+      }
+      assignment = getAssignmentsInfo(elements[i])
+
+      assignment.limit = getLimit(limit)
+      assignment_contents.push(assignment)
     }
-    console.log(limits)
+
+    console.log(assignment_contents)
   }, 5000)
+}
+
+const days = ["期限: 日曜日", "期限: 月曜日", "期限: 火曜日", "期限: 水曜日", "期限: 木曜日", "期限: 金曜日", "期限: 土曜日"]
+
+function getLimit(limit) {
+  console.log(limit)
+  if (days.includes(limit))
+    return getDataFromDay(limit)
+  if (limit === "明日")
+    return getTomorrowDate()
+  if (limit === "期限なし")
+    return limit
+
+  return limit.slice(4)
+}
+
+function getAssignmentsInfo(element) {
+  let name = element.getElementsByClassName("YVvGBb")[0].getElementsByClassName("onkcGd YVvGBb Evt7cb jp5EVb")[0].textContent
+
+  let subject = element.getElementsByClassName("YVvGBb")[0].getElementsByClassName("QRiHXd")[0].textContent
+  
+  if (subject.includes("期限"))
+    subject = subject.split("期限:")[0].slice(0, -3)
+
+  return {name, subject}
+}
+
+function getTomorrowDate() {
+  let today = new Date();
+  today.setDate(today.getDate() + 1)
+
+  return `${today.getMonth() + 1}月${today.getDate()}日`
+}
+
+function getDataFromDay(day) {
+  let dayIndex = days.indexOf(day)
+
+  let today = new Date();
+  console.log(today)
+
+  let dayToday = today.getDay();
+
+  if (dayIndex < dayToday) {
+    dayIndex += 7
+  }
+
+  let dayDiff = dayIndex - dayToday
+
+  today.setDate(today.getDate() + dayDiff)
+
+  return `${today.getMonth() + 1}月${today.getDate()}日`
 }
 
 loadElement()
