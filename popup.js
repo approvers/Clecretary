57d15441c9b60a02a1046463049def44855c6d0c
isso0424 <isso04240129@gmail.com>
2020-07-04 07:30:00 +0900

    [Add]タイトルの表示と使い方の誘導を追加

--- popup.js
+++ popup.js
@@ -3,7 +3,9 @@ let assignments
 function update() {
   let element = "";
 
-  console.log(assignments)
+  if (assignments == null) {
+    document.getElementById("app").innerHTML = "<h3>課題がインポートされていません</h3><h4>TODOの画面でリロードすることで課題を更新します</h4>"
+  }
   
   assignments.forEach((value, index) => {
     element += `<div class="task" id=assignment${index}><p>name: ${value.name}</p><p>limit: ${value.limit}</p><p>subject: ${value.subject}</p></div>`
@@ -21,11 +23,6 @@ function refresh() {
   assignments = chrome.extension.getBackgroundPage().assignments;
 
   update()
-}
-
-function moveToAssignment(url) {
-  console.log(url)
-  chrome.tabs.create({url})
 }
 
 chrome.storage.sync.get("assignments", (result) => {assignments = result.assignments; update()})
